6820a15a0d5bdde3dfd1428ab2b2be5d98d4587d
Alasil1 <120193939+Alasil1@users.noreply.github.com>
2024-10-09 10:43:28 +0300

    .

--- back.py
+++ back.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    
+    return f"welcome {name} your email: {email} has been received successfully"
 if __name__ == '__main__':
     app.run(debug=True)
